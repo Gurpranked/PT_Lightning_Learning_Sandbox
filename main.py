62568326d0b19290c22c6fecb92070fffbbbdc4b
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-05-09 19:26:43 -0400

    chore: Tensorboard logs geneated for training run

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ import torch.nn.functional as F
 from torchvision.transforms import v2
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
+from torchmetrics import Accuracy
+
+# W&B
+import wandb
+
+# Dotenv
+from dotenv import load_dotenv
 
 # PyTorch Lightning imports
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
+import lightning as pl
+from lightning import Trainer
+from lightning.pytorch.loggers import TensorBoardLogger
+load_dotenv()
 
 # Hyperparams
 BATCH_SIZE=32
@@ -24,67 +33,80 @@ INPUT_SIZE=784 # 28x28
 # Replace nn.Module with pl.LightningModule
 # pl.LightningModule is a superset of nn.Module
 class NN(pl.LightningModule): 
-    def __init__(self, input_size: int, hidden_size: int, num_classes: int, max_steps: int):
+    def __init__(self, input_size: int, hidden_size: int, num_classes: int):
         super(NN, self).__init__()
         self.input_size = input_size
         self.hidden_size  = hidden_size
         self.num_classes = num_classes
         self.validation_step_outputs = []
-        self.max_steps = max_steps
+        
+        self.loss_fn = F.cross_entropy
+        self.accuracy = Accuracy(task="multiclass", num_classes=10)
 
         self.l1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.l2 = nn.Linear(hidden_size, num_classes)
 
+        # W&B Logging
+        # Saves to self.hparams which are auto-logged by W&B
+        self.save_hyperparameters()
+
     # Returns raw logits (non-normalized) with no sigmoid or softmax applied
     def forward(self, x):
         return self.l2(self.relu(self.l1(x)))
     
-    # Definition of single batch forward pass over training
-    def training_step(self, batch, batch_index):
+    def _get_preds_loss_accuracy(self, batch):
+        """Helper function, train/valid/test steps are similar"""
         images, labels = batch
         images = images.reshape(-1, 28*28)
-        max_steps = self.max_steps
+        logits = self(images)
+        preds = torch.argmax(logits, dim=1)
+        loss = self.loss_fn(logits, labels)
+        acc = self.accuracy(preds, labels)
+        return preds, loss, acc
 
-        y_hat = self(images)
-        loss = F.cross_entropy(y_hat, labels)
+    # Definition of single batch forward pass over training
+    def training_step(self, batch, batch_index):
+        """Returns loss from a single batch"""
+        _, loss, acc = self._get_preds_loss_accuracy(batch)
 
-        tenserboard_logs = {'train_loss': loss}
+        # Logging
+        self.log("train_loss", loss, logger=True)
+        self.log("train_accuracy", acc, logger=True)
+        return loss
+        
+    def validation_step(self, batch, batch_idx):
+        """Logging metrics"""
+        preds, loss, acc = self._get_preds_loss_accuracy(batch)
 
-        return {'loss': loss, 'log': tenserboard_logs}
-    
-    # Obtain training dataloader
+        # Logging
+        self.log("val_loss", loss, logger=True)
+        self.log("val_accuracy", acc, logger=True)
+        return preds
+
     def train_dataloader(self):
+        """Provides the configured dataloader object for training"""
         train_dataset = MNIST(root="./data", train=True, download=True, transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]))
         train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
         return train_loader
     
     def val_dataloader(self):
+        """Provides the configured dataloader object for validation"""
         test_dataset = MNIST(root="./data", train=False, transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]))
         test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=False)
         return test_loader
-    
-    def validation_step(self, batch, batch_idx):
-        images, labels = batch
-        images = images.reshape(-1, 28 * 28)
-
-        # Forward pass
-        y_hat = self(images)
-
-        loss = F.cross_entropy(y_hat, labels)
-        self.validation_step_outputs.append({'val_loss': loss})  
-
-    def on_validation_epoch_end(self):
-        avg_loss = torch.stack([x['val_loss'] for x in self.validation_step_outputs]).mean()
-        tensorboard_logs = {'avg_val_loss': avg_loss}
-        return {'val_loss': avg_loss, 'log': tensorboard_logs}
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr = LR)
 
 if __name__ == '__main__':
-    model = NN(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES, max_steps=10)
-    trainer = Trainer(max_epochs=NUM_EPOCHS, deterministic=True)
+
+    # Configure logger
+    logger = TensorBoardLogger("tb_logs", name="PT-Lightning-MNIST")
+
+    # Define model and train
+    model = NN(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES)
+    trainer = Trainer(max_epochs=NUM_EPOCHS, logger=logger)
     trainer.fit(model)
 
     # TODO: Look into implementing Lighting DDP for distributed training across mutliple machines 
